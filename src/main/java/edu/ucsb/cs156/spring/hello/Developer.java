f61010e5b9b71bc8c7ffcd27bc2483a16cc6b487
Fern4ndo99 <fvelazquez4613@gmail.com>
2025-10-07 18:15:50 -0700

    xy - updated with actual github id

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -16,10 +16,7 @@ public class Developer {
      */
 
     public static String getName() {
-        // TODO: Change this to your name
-        // You may use just the name that is used on <https://bit.ly/cs156-f25-teams>
-        // i.e. your first name, or your first and initial of last name
-        return "Chris G.";
+        return "Fernando";
     }
 
     /**
@@ -29,7 +26,7 @@ public class Developer {
 
     public static String getGithubId() {
         // TODO: Change this to your github id
-        return "cgaucho";
+        return "fvelazquez";
     }
 
     /**
@@ -38,14 +35,13 @@ public class Developer {
      */
     
     public static Team getTeam() {
-        // TODO: Change this to your team name
-        Team team = new Team("f25-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("f25-14");
+        team.addMember("Fernando");
+        team.addMember("Keyi");
+        team.addMember("Catherine");
+        team.addMember("Pratima");
+        team.addMember("Daniel");
+        team.addMember("Logan");
         return team;
     }
 }
